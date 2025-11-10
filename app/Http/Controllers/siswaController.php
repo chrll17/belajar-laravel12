5f541de84eef0b7dbc5f90c429b5cf543469510d
chrll17 <119146753+chrll17@users.noreply.github.com>
2025-11-11 00:07:04 +0700

    membuat siswacontroller -> mengedit route agar mengirim data lewat controller

---
+++ app/Http/Controllers/siswaController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Siswa;
+use Illuminate\Http\Request;
+
+class siswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // $data_siswa = [
+        //     ['nama'=>'irul','nilai'=>80,'id'=>'001'],
+        //     ['nama'=>'anwar','nilai'=>50,'id'=>'002']
+        // ];
+
+        // $data_siswa=Siswa::all(); //mengambil semua data dari model (model mengambil data dari database)
+
+        $data_siswa=Siswa::orderby('nilai','desc')->get(); //mengambil data dari model (model mengambil data dari database) dari urutan nilai terbesar
+
+        return view('siswa.index', ['data_siswa' => $data_siswa]); //mengirim data ke view dengan array asosiatif
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $siswa=Siswa::findorfail($id); //menangkap data id yg dikirim dari siswa.index
+        return view('siswa.id', ['siswa' => $siswa]); //mengirim data ke view
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\siswaController;
 use App\Models\Siswa;
 use Illuminate\Support\Facades\Route;
 
@@ -12,18 +13,7 @@ Route::get('/home', function(){
 
 
 
-Route::get('/siswa', function(){
-    // $data_siswa = [
-    //     ['nama'=>'irul','nilai'=>80,'id'=>'001'],
-    //     ['nama'=>'anwar','nilai'=>50,'id'=>'002']
-    // ];
-
-    // $data_siswa=Siswa::all(); //mengambil semua data dari model
-
-    $data_siswa=Siswa::orderby('nilai','desc')->get(); //mengambil data dari model (model mengambil data dari database) dari urutan nilai terbesar
-
-    return view('siswa.index', ['data_siswa' => $data_siswa]); //mengirim data ke view dengan array asosiatif
-});
+Route::get('/siswa', [siswaController::class, 'index']);
 // Route::get('/siswa', function(){
 //     $nama='irul';
 //     return view('siswa.index', compact('nama')); //mengirim data ke view dengan COMPACT
@@ -32,7 +22,4 @@ Route::get('/siswa', function(){
 //     return view('siswa.index')->with('nama','irul'); //mengirim data ke view dengan WITH
 // });
 
-Route::get('/siswa/{id}', function($id){
-    $siswa=Siswa::findorfail($id); //menangkap data id yg dikirim dari siswa.index
-    return view('siswa.id', ['siswa' => $siswa]); //mengirim data ke view 
-});
+Route::get('/siswa/{id}', [siswaController::class, 'show']);
