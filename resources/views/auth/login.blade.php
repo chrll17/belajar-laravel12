5bd121f8ff446d9d0460e8e3e8752da9fd4d3b35
chrll17 <119146753+chrll17@users.noreply.github.com>
2025-11-17 15:04:45 +0700

    edit auth.login & auth.register untuk membuat form login & form register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,4 +3,15 @@
         login
     </x-slot:title>
     <h1>login</h1>
+    <form action="{{ route('auth.login') }}" method="post">
+        @csrf
+        <label for="email">email :</label>
+        <input type="email" name="email" id="email"><br>
+
+        <label for="password">password :</label>
+        <input type="password" name="password" id="password"><br>
+
+        <button type="submit">login</button>
+    </form>
+    belum punya akun? <a href="{{ route('auth.register') }}">register</a>
 </x-auth-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,4 +3,21 @@
         register
     </x-slot:title>
     <h1>register</h1>
+    <form action="{{ route('auth.register') }}" method="post">
+        @csrf
+        <label for="nama">nama :</label>
+        <input type="nama" name="nama" id="nama"><br>
+
+        <label for="email">email :</label>
+        <input type="email" name="email" id="email"><br>
+
+        <label for="password">password :</label>
+        <input type="password" name="password" id="password"><br>
+
+        <label for="password2">confirm password :</label>
+        <input type="password" name="password2" id="password2"><br>
+
+        <button type="submit">register</button>
+    </form>
+    sudah punya akun? <a href="{{ route('auth.login') }}">login</a>
 </x-auth-layout>
