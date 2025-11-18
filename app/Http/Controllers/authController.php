f59e72df2050f9e9a840d47142e12ca32837b896
chrll17 <119146753+chrll17@users.noreply.github.com>
2025-11-18 07:22:24 +0700

    edit authcontroller bagian register -> edit auth.register bagian error -> edit authcontroller bagian query database,autentikasi

--- app/Http/Controllers/authController.php
+++ app/Http/Controllers/authController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class authController extends Controller
 {
@@ -15,7 +17,17 @@ class authController extends Controller
     public function login(){
         
     }
-    public function register(){
-        
+    public function register(Request $request){
+        $validated=$request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        $user=User::create($validated); //menyimpan ke database
+
+        Auth::login($user); //autentikasi
+
+        return redirect()->route('siswa.index');
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,16 +6,25 @@
     <form action="{{ route('auth.register') }}" method="post">
         @csrf
         <label for="nama">nama :</label>
-        <input type="nama" name="nama" id="nama"><br>
+        <input type="text" name="name" id="nama" value="{{ old('name') }}">
+        @error('name')
+            {{ $message }}
+        @enderror<br>
 
         <label for="email">email :</label>
-        <input type="email" name="email" id="email"><br>
+        <input type="email" name="email" id="email" value="{{ old('email') }}">
+        @error('email')
+            {{ $message }}
+        @enderror<br>
 
         <label for="password">password :</label>
-        <input type="password" name="password" id="password"><br>
+        <input type="password" name="password" id="password">
+        @error('password')
+            {{ $message }}
+        @enderror<br>
 
         <label for="password2">confirm password :</label>
-        <input type="password" name="password2" id="password2"><br>
+        <input type="password" name="password_confirmation" id="password2"><br>
 
         <button type="submit">register</button>
     </form>
